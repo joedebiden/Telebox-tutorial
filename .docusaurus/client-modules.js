6db7a004064c028129dafec5f8dd884ed46061ad
evan <evan@>
2025-02-16 01:24:36 +0100

    init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/evan/Documents/projets-perso/Telebox-tutorial/node_modules/infima/dist/css/default/default.css"),
+  require("/home/evan/Documents/projets-perso/Telebox-tutorial/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/evan/Documents/projets-perso/Telebox-tutorial/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/evan/Documents/projets-perso/Telebox-tutorial/src/css/custom.css"),
+];
